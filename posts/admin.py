775e880ea5abfdc5fde92ad0aa21564dd9112005
Denis S <makafonov@gmail.com>
2020-07-27 09:42:31 +0500

    Fixed typo

--- posts/admin.py
+++ posts/admin.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk", "text", "pub_date", "author")
-    # добавляем интерфейс для поиска по тексту постов
-    search_fields = ("text",)
-    # добавляем возможность фильтрации по дате
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"  # это свойство сработает для всех колонок: где пусто - там будет эта строка
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 
 class GroupAdmin(admin.ModelAdmin):
@@ -19,6 +15,5 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
